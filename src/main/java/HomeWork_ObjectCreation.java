e06e64c678d5322cc450bf730b2da6387ca0a145
Evgenii Sirotin <esirot@esirot-c02qd038g8wm.attlocal.net>
2020-04-07 22:43:25 -0700

    Second

--- src/main/java/HomeWork_ObjectCreation.java
+++ src/main/java/HomeWork_ObjectCreation.java
@@ -1,4 +1,25 @@
 public class HomeWork_ObjectCreation {
 
+    class Object{
+        int i=5;
+    }
+    Object o1=new Object();
+    Object o2=new Object();
+    public void result(){
+        o1.i=10;
+        o2.i=20;
+        o1=o2;
+        //теперь о1 имеет идентичную ссылку как и о2 равное  20
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ src/main/java/QuestionFromGoogle.java
@@ -0,0 +1,27 @@
+// Вопрос заданный на последнем интервью
+
+public class QuestionFromGoogle {
+
+    public static class Calculator {
+        int num = 100;
+
+        public void calc(int num) {
+           this.num = num * 10;
+        }
+
+        public void printNum() {
+            System.out.println(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator obj = new Calculator();
+        obj.calc(2);
+        obj.printNum();
+    }
+//    Варианты ответа были
+////    1)20
+////    2)100
+////    3)1000
+////    4)2
+}
